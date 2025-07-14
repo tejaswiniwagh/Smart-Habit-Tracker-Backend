1605cd477d5299567a4298cca93e1bdd7996a58a
sakshimindcodes <sakshib@mindcodes.in>
2025-07-14 17:17:07 +0530

    .env file creation ,database setup

--- controllers/habitController.js
+++ controllers/habitController.js
@@ -17,4 +17,39 @@ exports.getHabits = (req, res) => {
   });
 };
 
-// Other methods (update, delete, track, stats)... same as before
+exports.updateHabit = (req, res) => {
+  const { id } = req.params;
+  const { name, type, goal_duration } = req.body;
+  const sql = 'UPDATE Habits SET name = ?, type = ?, goal_duration = ? WHERE id = ? AND user_id = ?';
+  db.query(sql, [name, type, goal_duration, id, req.user.id], (err) => {
+    if (err) return res.status(500).send(err);
+    res.json({ message: 'Habit updated' });
+  });
+};
+
+exports.deleteHabit = (req, res) => {
+  const { id } = req.params;
+  const sql = 'DELETE FROM Habits WHERE id = ? AND user_id = ?';
+  db.query(sql, [id, req.user.id], (err) => {
+    if (err) return res.status(500).send(err);
+    res.json({ message: 'Habit deleted' });
+  });
+};
+
+exports.trackHabit = (req, res) => {
+  const { id } = req.params;
+  const sql = 'INSERT INTO HabitTracking (habit_id, date) VALUES (?, CURDATE())';
+  db.query(sql, [id], (err) => {
+    if (err) return res.status(500).send(err);
+    res.json({ message: 'Habit tracked for today' });
+  });
+};
+
+exports.getHabitStats = (req, res) => {
+  const { id } = req.params;
+  const sql = 'SELECT COUNT(*) AS streak FROM HabitTracking WHERE habit_id = ?';
+  db.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.json({ streak: results[0].streak });
+  });
+};
--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+  const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Tejaswini@10',  // your MySQL password here if set
-  database: 'habit_tracker'
-});
+  const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Yash@2007',  // your MySQL password here if set
+    database: 'habit_tracker'
+  });
 
-module.exports = db;
+  module.exports = db;
